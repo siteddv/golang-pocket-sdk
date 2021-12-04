1a551b74b5583d14b3972660990007f40307bdf7
Dimitrii Vinogradov <siteddv@gmail.com>
2021-12-05 00:02:41 +0600

    started declaring constants

---
+++ pocket.go
@@ -0,0 +1,42 @@
+package golang_pocket_sdk
+
+import "time"
+
+const (
+	host         = "https://getpocket.com/v3"
+	authorizeUrl = "https://getpocket.com/auth/authorize?request_token=%s&redirect_uri=%s"
+
+	endpointAdd          = "/add"
+	endpointRequestToken = "/oauth/request"
+	endpointAuthorize    = "/oauth/authorize"
+
+	// xErrorHeader used to parse error message from Headers on non-2XX responses
+	xErrorHeader = "X-Error"
+
+	defaultTimeout = 5 * time.Second
+)
+
+type (
+	requestTokenRequest struct {
+		ConsumerKey string `json:"consumer_key"`
+		RedirectURI string `json:"redirect_uri"`
+	}
+
+	authorizeRequest struct {
+		ConsumerKey string `json:"consumer_key"`
+		Code        string `json:"code"`
+	}
+
+	AuthorizeResponse struct {
+		AccessToken string `json:"access_token"`
+		Username    string `json:"username"`
+	}
+
+	addRequest struct {
+		URL         string `json:"url"`
+		Title       string `json:"title,omitempty"`
+		Tags        string `json:"tags,omitempty"`
+		AccessToken string `json:"access_token"`
+		ConsumerKey string `json:"consumer_key"`
+	}
+)
